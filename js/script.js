3187ea701c713ed5fb4973c0742ba8f396bdda9a
Demar English <demarenglish@Mac.attlocal.net>
2024-10-18 00:56:31 -0400

    Added the create grid function for the default grid

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const createGrid = function () {
+const createGrid = function (cols, rows) {
     const gridContainer = document.querySelector(".etch-a-sketch");
 
-    for (let cell = 0; cell < 16 * 16; cell++) {
+    for (let cell = 0; cell < cols * rows; cell++) {
         const createCell = document.createElement("div");
         createCell.setAttribute("class", "cell");
         createCell.style.border = "1px solid black";
-        createCell.style.height = `calc(${cell * 100}%)`;
-        createCell.style.width = `calc(${cell * 100}%)`;
+        createCell.style.height = `calc(100% / ${cols})`;
+        createCell.style.width = `calc(100% / ${rows})`;
         gridContainer.appendChild(createCell);
 
     }
@@ -14,4 +14,4 @@ const createGrid = function () {
     return gridContainer;
 }
 
-createGrid();
+createGrid(16, 16);
